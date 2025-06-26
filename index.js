9f99ff65a4aed45e8c208079d274b913a8c6d6f8
Eishal <eishal.cecos@gmail.com>
2025-06-26 22:56:06 +0500

    p
    p

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ app.get('/', (req, res) => {
   res.send('🚀 Backend is running and ready!');
 });
 const PORT = process.env.PORT || 5000;
+console.log("⚡ Trying to start server on port", PORT);
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
