66304d176ca7844005ee8407fa2a3179b5ef19dc
Eishal <eishal.cecos@gmail.com>
2025-06-26 22:26:20 +0500

    p

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/api/test-db', async (req, res) => {
 app.get('/', (req, res) => {
   res.send('🚀 Backend is running and ready!');
 });
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
