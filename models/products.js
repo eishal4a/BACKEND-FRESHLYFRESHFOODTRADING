c9d36f4b1289741ff08180c953af6fdd4bd88bd7
Eishal <eishal.cecos@gmail.com>
2025-06-29 21:38:00 +0500

    p
    p

--- models/products.js
+++ models/products.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  category: String,
-  image: String,
-  description: String
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String, required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Product', productSchema);
