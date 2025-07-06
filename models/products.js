935e9d37a4f592a8d87173703daf86392ce3c1c3
Eishal <eishal.cecos@gmail.com>
2025-07-06 14:29:55 +0500

    p
    p

--- models/products.js
+++ models/products.js
@@ -1,13 +1,12 @@
-// models/Product.js
 const mongoose = require('mongoose');
 
-const ProductSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  quantity: { type: Number, default: 1 },
-  image: { type: String, required: true }, // URL or relative path
-  description: { type: String, required: true },
-  category: { type: String, enum: ['meat', 'fruit'], required: true },
-});
+  image: { type: String, required: true },
+  category: { type: String, required: true },
+  description: { type: String, default: '' },
+  quantity: { type: Number, default: 1 }
+}, { timestamps: true });
 
-module.exports = mongoose.model('Product', ProductSchema);
+module.exports = mongoose.model('Product', productSchema);
