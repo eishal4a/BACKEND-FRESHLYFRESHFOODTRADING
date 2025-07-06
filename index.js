0a402d360241be892defd485dc4c7ee7ae71c3e1
Eishal <eishal.cecos@gmail.com>
2025-07-06 14:29:13 +0500

    p
    p

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express();
 
 app.use(cors({
   origin: [
-    "http://localhost:3000",
-    "https://e-commerce-website-git-main-ash-d0707d97.vercel.app/"
-  ],
+  "http://localhost:3000",
+  "https://e-commerce-website-git-main-ash-d0707d97.vercel.app"
+],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
